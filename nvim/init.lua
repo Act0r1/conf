c73fb0d4c5d50bf729888f9a61955c654bc63595
Act0r1 <gizatullininsaf25@gmail.com>
2024-04-24 19:28:28 +0300

    plugin update

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,10 @@ require("user.keymaps")
 require("user.options")
 require("user.packer")
 require('transparent').clear_prefix('NvimTree')
+vim.api.nvim_set_keymap('i', '<A-j>',"copilot#Accept('<CR>')", {noremap=true, silent=true, expr=true})
+
+
+
+
+
+
