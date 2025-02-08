e2b690fbcfd3e27198fc53c357652d7b4715d2d6
actor <gizatullininsaf25@gmail.com>
2025-02-08 22:39:48 +0300

    hypr: kitty & wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,36 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+    automatically_reload_config = true,
+    enable_tab_bar = false,
+    window_close_confirmation = "NeverPrompt",
+    window_decorations = "NONE",
+    font = wezterm.font("Comic Mono"),
+    font_size = 14.5,
+    background = {
+        {
+            source = {
+                File = "/Users/yea8er/Pictures/alone.png",
+            },
+            hsb = {
+                hue = 1.0,
+                saturation = 1.0,
+                brightness = 0.1,
+            },
+            width = "100%",
+            height = "100%",
+        },
+        -- {
+        --     source = {
+        --         Color = "#282c35",
+        --     },
+        --     width = "100%",
+        --     height = "100%",
+        --     opacity = 0.5,
+        -- },
+    },
+}
+config.color_scheme = "Tokyo Night"
+return config
